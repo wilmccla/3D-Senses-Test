a3c35c8c6ab2366f8d4f0b89fb090b8da325f1d3
Will McClay <wilmccla@uat.edu>
2019-05-14 17:43:42 -0700

    Player Movement Testing

---
+++ 3D Senses Test/Assets/Scripts/InputController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputController : MonoBehaviour
+{
+    public GameObject player; //Player gameobject
+    public Transform playerTf;  //Player Transform Variable
+    public PlayerMover mover; //Player mover script
+
+    private Vector3 directionToMove;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Initializing Variables
+        playerTf = GetComponent<Transform>();
+        mover = player.GetComponent<PlayerMover>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    /*    //start the game with the character not moving
+        directionToMove = Vector3.zero;
+
+        //if the player presses W, move the character forward
+        if (Input.GetKey(KeyCode.W))
+        {
+            directionToMove += playerTf.forward;
+        }
+
+        //if the player presses S, move the character backwards
+        if (Input.GetKey(KeyCode.S))
+        {
+            directionToMove -= playerTf.forward;
+        }
+
+        //if the player presses D, strafe the player to the right
+        if (Input.GetKey(KeyCode.D))
+        {
+            directionToMove += playerTf.right;
+        }
+
+        //if the player presses A, strafe the player to the left
+        if (Input.GetKey(KeyCode.A))
+        {
+            directionToMove -= playerTf.right;
+        }
+
+        //After checking inputs, tell the mover to move in that direction
+        mover.PlayerMove(directionToMove); */
+    }
+}
---
+++ 3D Senses Test/Assets/Scripts/PlayerMover.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMover : MonoBehaviour
+{
+    //Declaring PlayerMover variables
+    private CharacterController characterController;
+    private Vector3 moveDirection = Vector3.zero;
+
+    public float speed = 6f;
+    public float jumpSpeed = 8f;
+    public float gravity = 20f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Declaring the character controller
+        characterController = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (characterController.isGrounded)
+        {
+            //Getting input on 
+            moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
+
+            moveDirection *= speed;
+
+            if (Input.GetKey(KeyCode.Space))
+            {
+                moveDirection.y = jumpSpeed;
+            }
+        }
+
+        moveDirection.y -= gravity * Time.deltaTime;
+
+        characterController.Move(moveDirection * Time.deltaTime);
+    }
+}
