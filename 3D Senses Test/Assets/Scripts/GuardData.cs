3fd01f130291e26f8f62ab53d8d718db6434c08d
William McClay <wilmccla@uat.edu>
2019-05-16 19:09:08 -0700

    Lots of patrol code

---
+++ 3D Senses Test/Assets/Scripts/GuardData.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class GuardData : MonoBehaviour
+{
+    [Header("Components")]
+    public GuardMover mover;
+    public NavMeshAgent nav;
+
+    [Header("Variables")]
+    public float moveSpeed;
+    public Transform[] waypoints;
+    public int currentWaypointIndex = 0;
+
+    void Start()
+    {
+        nav = GetComponent<NavMeshAgent>();
+    }
+}
---
+++ 3D Senses Test/Assets/Scripts/GuardMover.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class GuardMover : MonoBehaviour
+{
+    // Guard Mover Variables
+    public GuardData data;
+    private CharacterController characterController;
+
+    void Start()
+    {
+        //Initializing Variables
+        characterController = GetComponent<CharacterController>();
+        //Start the patrol
+        MoveToNextWaypoint();
+    }
+
+    void Update()
+    {
+        if (!data.nav.pathPending && data.nav.remainingDistance < 0.5f)
+        {
+            MoveToNextWaypoint();
+        }
+    }
+
+    void MoveToNextWaypoint()
+    {
+        if (data.waypoints.Length > 0)
+        {
+            //Guard's destination is the current waypoint
+            data.nav.destination = data.waypoints[data.currentWaypointIndex].position;
+
+            //Increment waypoint
+            data.currentWaypointIndex++;
+            //Set Index back to zero when it goes over the max length
+            data.currentWaypointIndex %= data.waypoints.Length;
+        }
+    }
+}
